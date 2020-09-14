d20a9a3c1bdd11291e7f372e95f3b10016094586
John Maguire <jmaguire@insightsquared.com>
2020-09-13 20:38:14 -0400

    allow all origins for now

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const express = require('express');
 
 const app = express()
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 app.use(express.json())
+
 
 app.get('/', (req, res) => {
   return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
